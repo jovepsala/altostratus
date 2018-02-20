97b4e73f32384e9ebb57b7373db1be5514e6f5ee
jovepsal <jovepsal@cs.helsinki.fi>
2018-02-20 16:35:21 +0200

    test

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,5 @@
+import time
+import datetime
 import socket
 from flask import Flask
 
@@ -6,8 +8,23 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
 
-    return "Hello World! Greetings from "+socket.gethostname()+"!\n"
+    timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+    line = timestamp + "\t" + socket.gethostname() + "\n"
 
+    file = open("/mnt/log.txt", "a+")
+    file.write(line)
+
+    file.seek(0, 0)
+    content = file.read()
+
+    file.close()
+
+    return content
 
 if __name__ == "__main__":
     application.run()
+
+
+
+
+
